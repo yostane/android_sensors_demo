0b01ca67ccea9dbda40ea1b7ef3bf876a70830b0
yostane <yostane@yahoo.fr>
2019-06-07 12:05:45 +0200

    added logs

--- android_clicodrome/app/src/main/java/fr/efficom/formation/cpw_mpw/MainActivity.kt
+++ android_clicodrome/app/src/main/java/fr/efficom/formation/cpw_mpw/MainActivity.kt
@@ -1,6 +1,7 @@
 package fr.efficom.formation.cpw_mpw
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         plus1.setOnClickListener {
+            //verbose
+            Log.v("plus1", "compteur= $compteur")
+            //debug
+            Log.d("plus1", "compteur= $compteur")
+            //info
+            Log.i("plus1", "compteur= $compteur")
+            //warn
+            Log.w("plus1", "compteur= $compteur")
+            //error
+            Log.e("plus1", "compteur= $compteur")
             compteur += 1
             zoneTexte.text = "$compteur"
         }
@@ -24,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         plus2.setOnClickListener {
+            Log.d("plus2", "compteur2= $compteur2")
             compteur2 += 1
             zoneTexte2.text = "$compteur2"
         }
